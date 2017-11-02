896e67916b6786b9885d63bb1da164e8e771814e
Ashok Cutkosky <ashokc@cs.stanford.edu>
2017-11-01 20:09:21 -0700

    added freerex mirror descent

--- freerex.py
+++ freerex.py
@@ -98,6 +98,79 @@ class FreeRex(Optimizer):
         return loss
 
 
+
+class FreeRexMD(Optimizer):
+    """Implements FreeRex algorithm 
+        http://proceedings.mlr.press/v65/cutkosky17a.html.
+
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate analogue (specifies a point on optimal regret frontier) (default: 1/sqrt(5))
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+    """
+
+    def __init__(self, params, lr=1.0/np.sqrt(5.0), weight_decay=0):
+        defaults = dict(lr=lr, weight_decay=weight_decay)
+        super(FreeRexMD, self).__init__(params, defaults)
+
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['step'] = 0
+                state['one_over_eta_sq'] = p.data.new().resize_as_(p.data).fill_(EPSILON)
+                state['grad_sum'] = p.data.new().resize_as_(p.data).zero_()
+                state['log'] = p.data.new().resize_as_(p.data).zero_()
+                state['scaling'] = 1.0#p.data.new().resize_as_(p.data).fill_(1.0)
+                state['max_grad'] = p.data.new().resize_as_(p.data).fill_(1.0)
+                state['max_l2'] = EPSILON
+                state['center'] = p.data.clone()
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+
+                grad = p.grad.data
+                state = self.state[p]
+
+                state['step'] += 1
+
+                if group['weight_decay'] != 0:
+                    if p.grad.data.is_sparse:
+                        raise RuntimeError("weight_decay option is not compatible with sparse gradients ")
+                    grad = grad.add(group['weight_decay'], p.data)
+
+                state['max_grad'] = torch.max(state['max_grad'], torch.abs(grad))
+
+                state['max_l2'] = max(state['max_l2'], grad.norm(2))
+
+                state['scaling'] = min(state['scaling'], state['max_l2']/torch.sum(state['max_grad']))
+
+                state['grad_sum'].add_(grad)
+
+                state['one_over_eta_sq'] = torch.max(state['one_over_eta_sq'] + 2*grad.pow(2), state['max_grad'] * torch.abs(state['grad_sum']))
+
+                state['log'] -= grad/torch.sqrt(state['one_over_eta_sq'])
+
+                p.data = state['center'] + torch.sign(state['log']) * (torch.exp(torch.abs(state['log']))-1)
+
+        return loss
+
+
+
+
 class FreeRexSphere(Optimizer):
     """Implements FreeRex algorithm 
         http://proceedings.mlr.press/v65/cutkosky17a.html.
